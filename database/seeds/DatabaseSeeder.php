5bcf202c401ccbea1ba58e6b3e4faffff0cb530f
gomgomgit <thechocohead@gmail.com>
2020-03-30 20:36:56 +0700

    seeder repair

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -9,27 +9,8 @@ class DatabaseSeeder extends Seeder {
 	 * @return void
 	 */
 	public function run() {
-		for ($i = 0; $i < 5; $i++) {
-			Category::insert([
-				'name' => $fake->word,
-			]);
-		}
-		$rcate = Category::pluck('id');
-		for ($i = 0; $i < 5; $i++) {
-			Item::insert([
-				// 'category_id' => Category::all()->random()->id,
-				'category_id' => $fake->randomElement($rcate),
-				'name' => $fake->word,
-				'price' => ($fake->numberBetween($min = 3, $max = 99) * 1000),
-				'stock' => $fake->numberBetween($min = 1, $max = 99),
-			]);
-		}
-		for ($i = 0; $i < 5; $i++) {
-			Users::insert([
-				'name' => $faker->name,
-				'email' => $faker->email,
-				'password' => bcrypt(111111),
-			]);
-		}
+		$this->call(CategorySeeder::class);
+		$this->call(ItemSeeder::class);
+		$this->call(UserSeeder::class);
 	}
 }
