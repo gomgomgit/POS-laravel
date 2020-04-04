59b5ec49a8194ac4827711401d2c5e5da5b181b3
gomgomgit <thechocohead@gmail.com>
2020-04-04 08:50:07 +0700

    fixing

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('', function () {
 	return view('auth.login');
-})->name('login');
+});
 // Route::post('login', 'LogController@check');
 Route::get('logout', 'LogController@out');
 
