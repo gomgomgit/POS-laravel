76e9bb082f420d997ad60f8acfd1cf37136975c7
gomgomgit <thechocohead@gmail.com>
2020-04-06 08:00:11 +0700

    add laravolt avatar

--- resources/views/template/part/header.blade.php
+++ resources/views/template/part/header.blade.php
@@ -201,13 +201,13 @@
     <!-- User Account: style can be found in dropdown.less -->
     <li class="dropdown user user-menu">
       <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-        {{-- <img src="dist/img/user2-160x160.jpg" class="user-image" alt="User Image"> --}}
+        <img src="{{ Avatar::create(Request::user()->name)->toBase64() }}" class="user-image" alt="User Image">
         <span class="hidden-xs">{{Request::user()->name}}</span>
       </a>
       <ul class="dropdown-menu">
         <!-- User image -->
         <li class="user-header">
-          {{-- <img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image"> --}}
+          <img src="{{ Avatar::create(Request::user()->name)->toBase64() }}" class="img-circle" alt="User Image">
 
           <p>
             {{Request::user()->name}} - Web Developer
