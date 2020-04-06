b68a05fff1650984d7d19526b0a6b9a67ac227ab
gomgomgit <thechocohead@gmail.com>
2020-04-06 10:33:10 +0700

    prefix route

--- routes/web.php
+++ routes/web.php
@@ -21,37 +21,46 @@ Route::get('logout', 'LogController@out');
 
 Route::get('dashboard', 'DashboardController@index')->name('dashboard');
 
-Route::get('category', 'CategoryController@index')->name('haha');
-Route::get('category/add', function () {
-	return view('category.add');
+Route::prefix('category')->group(function () {
+	Route::get('', 'CategoryController@index')->name('haha');
+	Route::get('add', function () {
+		return view('category.add');
+	});
+	Route::post('store', 'CategoryController@store');
+	Route::get('delete/{id}', 'CategoryController@delete');
+	Route::get('edit/{id}', 'CategoryController@edit');
+	Route::post('update/{id}', 'CategoryController@update');
 });
-Route::post('category/store', 'CategoryController@store');
-Route::get('category/delete/{id}', 'CategoryController@delete');
-Route::get('category/edit/{id}', 'CategoryController@edit');
-Route::post('category/update/{id}', 'CategoryController@update');
 
-Route::get('item', 'ItemController@index')->name('login');
-Route::get('item/add', 'ItemController@add');
-Route::post('item/store', 'ItemController@store');
-Route::get('item/edit/{id}', 'ItemController@edit');
-Route::put('item/update/{id}', 'ItemController@update');
-Route::get('item/delete/{id}', 'ItemController@delete');
+Route::prefix('item')->group(function () {
+	Route::get('', 'ItemController@index')->name('login');
+	Route::get('add', 'ItemController@add');
+	Route::post('store', 'ItemController@store');
+	Route::get('edit/{id}', 'ItemController@edit');
+	Route::put('update/{id}', 'ItemController@update');
+	Route::get('delete/{id}', 'ItemController@delete');
+});
 
-Route::get('users', 'UsersController@index');
-Route::get('users/add', 'UsersController@add');
-Route::post('users/store', 'UsersController@store');
-Route::get('users/edit/{id}', 'UsersController@edit');
-Route::post('users/update/{id}', 'UsersController@update');
-Route::get('users/delete/{id}', 'UsersController@destroy');
+Route::prefix('users')->group(function () {
+	Route::get('', 'UsersController@index');
+	Route::get('add', 'UsersController@add');
+	Route::post('store', 'UsersController@store');
+	Route::get('edit/{id}', 'UsersController@edit');
+	Route::post('update/{id}', 'UsersController@update');
+	Route::get('delete/{id}', 'UsersController@destroy');
+});
 
-Route::get('orders', 'OrdersController@index');
-Route::get('orders/add', 'OrdersController@add');
-// Route::get('/orders/add', function () {
-// 	return view('orders/add');
-// })->where('any', '.*');
-Route::post('orders/store', 'OrdersController@store');
-Route::get('orders/details/{id}', 'OrdersController@details');
-Route::get('orders/delete/{id}', 'OrdersController@delete');
+Route::prefix('orders')->group(function () {
+	Route::get('', 'OrdersController@index');
+	Route::get('add', 'OrdersController@add');
+	// Route::get('orders/add', function () {
+	// 	return view('add');
+	// })->where('any', '.*');
+	Route::post('store', 'OrdersController@store');
+	Route::get('details/{id}', 'OrdersController@details');
+	Route::get('delete/{id}', 'OrdersController@delete');
+});
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
