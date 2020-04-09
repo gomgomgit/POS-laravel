b6a24b59f814ac0ff03ee84eff33430c51deb236
gomgomgit <thechocohead@gmail.com>
2020-04-09 16:25:43 +0700

    add role

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller {
 	/*
@@ -37,7 +38,7 @@ class LoginController extends Controller {
 	}
 
 	protected function authenticated($request, $user) {
-		if ($user->role == '2') {
+		if (Auth::user()->can('isStaff')) {
 			return redirect()->intended('orders/add'); //redirect to except panel
 		}
 
